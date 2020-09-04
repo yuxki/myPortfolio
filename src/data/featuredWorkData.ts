e4a8210b47cf7a8d937d9ac21cb45a04f911af45
yuxki <yukihiro.uejo@gmail.com>
2020-09-04 15:19:24 +0900

    20200904 #23 topPageContentのリファクタリング

---
+++ src/data/featuredWorkData.ts
@@ -0,0 +1,53 @@
+import { FeaturedWorkInfo } from 'featuredWork';
+
+export const graphicsDesignInfo: FeaturedWorkInfo = {
+	featuredWorkTitle: 'Graphic Design',
+	featuredWorkType: 'img',
+	featuredWorkImgSrc: [
+		'./public/tokyoHalloween.png'
+		, './public/pawoon.png'
+		, './public/flyingRabbit_wide.png'
+		, './public/interface_logo.png'
+	],
+	featuredWorkButtomMaxWidth: ['54px', '78px', '115px', '111px'],
+	featuredWorkImgElem: [],
+	featuredWorkLayout: 'single_img',
+}
+
+export const applicationDesignInfo: FeaturedWorkInfo = {
+	featuredWorkTitle: 'App Design',
+	featuredWorkType: 'img',
+	featuredWorkImgSrc: [
+		'./public/calcurate.png'
+		, './public/camera.png'
+		, './public/cookingRecipe.png'
+		, './public/game.png'
+		, './public/itemDetail.png'
+		, './public/mapDist.png'
+		, './public/musicLogin.png'
+		, './public/musicPlay.png'
+		, './public/readingBook.png'
+		, './public/snsProfile.png'
+		, './public/sportLogin.png'
+		, './public/weather.png'
+	],
+	featuredWorkButtomMaxWidth: [],
+	featuredWorkImgElem: [],
+	featuredWorkLayout: 'single_img',
+}
+
+export const threDGraphicsInfo: FeaturedWorkInfo = {
+	featuredWorkTitle: '3D Graphics',
+	featuredWorkType: 'img',
+	featuredWorkImgSrc: [
+		'./public/lightHouse_night.png'
+		, './public/lighthouse_back.jpg'
+		, './public/lighthouse_front.jpg'
+		, './public/lighthouse_right.jpg'
+		, './public/lightHouse_wire.jpg'
+		, './public/lightHouse_morning.png'
+	],
+	featuredWorkButtomMaxWidth: [],
+	featuredWorkImgElem: [],
+	featuredWorkLayout: 'double_img',
+}
