143540d8931c17907c9e5ed6cfb2d17fe6c94a3d
yuxki <yukihiro.uejo@gmail.com>
2020-09-07 14:14:42 +0900

    20200907 #23 rightAngleSingleStaticLayout

--- src/layout/rightAngleSingleStaticLayout.tsx
+++ src/layout/rightAngleSingleStaticLayout.tsx
@@ -4,7 +4,8 @@ import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/sty
 import IconButton from '@material-ui/core/IconButton';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import ImageIcon from '@material-ui/icons/Image';
-import FeaturedWorkTitle from "../featuredWorkTitle"
+import FeaturedWorkTitle from '../featuredWorkTitle';
+import { FeaturedWorkInfo } from 'featuredWork';
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -14,8 +15,8 @@ const useStyles = makeStyles((theme: Theme) =>
 			justifyContent: 'center',
 			height: 'calc(100vh + 80px)',
 			minHeight: '580px',
-			backgroundColor: '#F7F7F7',
 			margin: '0 calc(50% - 50vw)',
+			backgroundColor: '#F7F7F7',
 		},
 		rightAngleLayout: {
 			display: 'flex',
@@ -26,11 +27,11 @@ const useStyles = makeStyles((theme: Theme) =>
 		},
 		mainImageArea: {
 			display: 'flex',
+			alignItems: 'center',
+			flexDirection: 'column',
 			width: '583px',
 			height: '380px',
 			marginRight: theme.spacing(1),
-			alignItems: 'center',
-			flexDirection: 'column',
 			[theme.breakpoints.down('sm')]: {
 				width: '100%',
 				height: 'auto',
@@ -45,8 +46,10 @@ const useStyles = makeStyles((theme: Theme) =>
 			width: '583px',
 			height: '328px',
 			[theme.breakpoints.down('sm')]: {
+				// 画面の横幅の56％
 				width: 'calc(100vw * 0.56)',
 				maxWidth: '583px',
+				// 1920×1080の縦割合が0.5625のため、0.5625を乗算する
 				height: 'calc(100vw * 0.56 * 0.5625)',
 			},
 			[theme.breakpoints.down('xs')]: {
@@ -57,20 +60,20 @@ const useStyles = makeStyles((theme: Theme) =>
 		},
 		videocamIconArea: {
 			display: 'flex',
-			width: '100%',
-			height: theme.spacing(6),
 			justifyContent: 'center',
 			alignItems: 'center',
+			width: '100%',
+			height: theme.spacing(6),
 		},
 		movingBall: {
 			display: 'flex',
 			width: '12px',
 			height: '12px',
+			marginRight: '-8px',
 			borderRadius: '50%',
+			backgroundColor: 'rgb(242, 190, 34, 0.5)',
 			transitionDuration: '0.3s',
 			transitionTimingFunction: 'ease-out',
-			marginRight: '-8px',
-			backgroundColor: "rgb(242, 190, 34, 0.5)",
 		},
 		moveRightSide: {
 			transform: 'translateX(74px)',
@@ -82,7 +85,7 @@ const useStyles = makeStyles((theme: Theme) =>
 			marginRight: theme.spacing(3),
 		},
 		root: {
-			color: "#F2BE22",
+			color: theme.palette.primary.main,
 		},
 		hide: {
 			display: 'none',
@@ -160,8 +163,6 @@ const useStyles = makeStyles((theme: Theme) =>
 			height: '140px',
 			margin: theme.spacing(1, 0, 0, 0),
 			[theme.breakpoints.down('sm')]: {
-				width: '249px',
-				height: '140px',
 				margin: theme.spacing(0, 0, 1, 0),
 			},
 			[theme.breakpoints.down('xs')]: {
@@ -189,6 +190,14 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function RightAngleSingleStaticLayout(props) {
 	const classes = useStyles();
+	const featuredWorkInfo: FeaturedWorkInfo = props.featuredWorkInfo;
+	const title: string = featuredWorkInfo.featuredWorkTitle;
+
+	// 配列の一番最初の画像を、mainImageとして取得
+	const mainImage: string = featuredWorkInfo.featuredWorkImgSrc[0];
+	const squareImageArray: Array<string> = featuredWorkInfo.featuredWorkImgSrc.slice(1, 4);
+	const rectangleImageArray: Array<string> = featuredWorkInfo.featuredWorkImgSrc.slice(4);
+
 	const [isVideoModeOn, setIsVideoModeOn] = React.useState(false);
 	function handleVideoModeOn() {
 		setIsVideoModeOn(true);
@@ -197,16 +206,12 @@ export default function RightAngleSingleStaticLayout(props) {
 		setIsVideoModeOn(false);
 	}
 
-	const mainImage: string = props.imageInfoSrcList[0];
-	const squareImageArray: Array<string> = props.imageInfoSrcList.slice(1, 4);
-	const rectangleImageArray: Array<string> = props.imageInfoSrcList.slice(4);
-
 	return (
 		<div className={classes.backgroundArea}>
 			<div className={classes.rightAngleLayout}>
 				<div className={classes.mainImageArea}>
 					<img className={clsx(classes.mainImage, isVideoModeOn && classes.hide)} src={mainImage} />
-					<iframe className={clsx(classes.mainImage, classes.hide, isVideoModeOn && classes.appear)} src="https://www.youtube.com/embed/uUPppgbQGow?loop=1&rel=0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" frameBorder="0"></iframe>
+					<iframe className={clsx(classes.mainImage, classes.hide, isVideoModeOn && classes.appear)} src='https://www.youtube.com/embed/uUPppgbQGow?loop=1&rel=0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' frameBorder='0'></iframe>
 					<div className={classes.videocamIconArea}>
 						<div className={clsx(
 							classes.movingBall,
@@ -217,7 +222,7 @@ export default function RightAngleSingleStaticLayout(props) {
 							className={clsx(classes.modeIcon, classes.modeIconMargin)}
 							onClick={handleVideoModeOff}
 							classes={{ root: clsx(!isVideoModeOn && classes.root) }}
-							size="medium"
+							size='medium'
 						>
 							<ImageIcon />
 						</IconButton>
@@ -225,7 +230,7 @@ export default function RightAngleSingleStaticLayout(props) {
 							className={classes.modeIcon}
 							classes={{ root: clsx(isVideoModeOn && classes.root) }}
 							onClick={handleVideoModeOn}
-							size="medium"
+							size='medium'
 						>
 							<VideocamIcon />
 						</IconButton>
@@ -248,7 +253,7 @@ export default function RightAngleSingleStaticLayout(props) {
 					</div>
 				</div>
 				<div className={classes.titleArea}>
-					<FeaturedWorkTitle featuredWorkTitle={props.featuredWorkTitle} />
+					<FeaturedWorkTitle featuredWorkTitle={title} />
 				</div>
 			</div>
 		</div>
